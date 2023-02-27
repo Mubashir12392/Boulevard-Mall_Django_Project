714591f752472cd64706e3b52de64ed11f744a1e
Usman4862 <um4862@gmail.com>
2023-02-27 02:10:24 -0800

    modify imported views, add paths, add static urls

--- boulevard_mall/urls.py
+++ boulevard_mall/urls.py
@@ -15,17 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from shop.views import pakistan_store
-from shop.views import shop_categories
-from shop.views import grocery
-from shop.views import makeup
-from shop.views import phone
+from django.conf import settings
+from django.conf.urls.static import static
+from shop.views import pakistan_store, shop_categories, grocery, makeup, phone, skin_care, perfume, home_appliances
+from core.views import  home_core
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', pakistan_store, name='homepage'),
     path('home/shop/', shop_categories, name='shop_page'),
     path('home/shop/grocery/', grocery, name='grocery_shop'),
     path('home/shop/makeup/', makeup, name='makeup_shop'),
-    path('home/shop/mobilephone/', phone, name='phone_shop')
-
+    path('home/shop/mobilephone/', phone, name='phone_shop'),
+    path('home/shop/skincare/', skin_care, name='skin-care'),
+    path('home/shop/perfume/', perfume, name='perfume'),
+    path('home/shop/home/appliances', home_appliances, name='home-appliances'),
+    path('', home_core, name='home-core'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_URL)
+ 
