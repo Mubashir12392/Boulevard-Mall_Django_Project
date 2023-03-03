b027e32db9bb4874be56ed1cd87a07ab87fbd25d
Mubashir12392 <mubashirhaider01@gmail.com>
2023-03-04 00:19:28 +0500

    add new views

--- shop/views.py
+++ shop/views.py
@@ -1,56 +1,21 @@
 from django.shortcuts import render,reverse, redirect
-from shop.models import Groceries, Makeup, Mobile_phones, Skincare, Perfume, HomeAppliances
+from shop.models import Makeup, Mobile_phones, Skincare, Perfume, HomeAppliances
 # Create your views here.
 
-def pakistan_store(request):
-    return render(request, 'main_page.html')
+def boulevard(request):
+    return render(request, 'index.html')
 
-def shop_categories(request):
-    return render(request, 'shop_page.html')
+def shoppingcart(request):
+    return render(request, 'cart.html')
 
-def grocery(request):
-    groceries = Groceries.objects.all()
-    context = {
-        'groceries' : groceries
-    }
-    return render(request, 'grocery.html', context=context)
+def shopproducts(request):
+    return render(request,'shop.html')
 
-def makeup(request):
-    makeup = Makeup.objects.all()
-    context = {
-        'makeup' : makeup
-    }
-    return render(request, 'makeup.html', context=context )
+def checkout(request):
+    return render(request,'checkout.html')
 
-def phone(request):
-    phone = Mobile_phones.objects.all()
-    context = {
-        'phone' : phone
-    }
-    return render(request, 'phone.html', context=context)
+def feature(request):
+    return render(request, 'feature.html')
 
-
-def skin_care(request):
-    product = Skincare.objects.all()
-    context = {
-        'product' : product
-    }
-    return render(request, 'skin_care.html', context=context)
-
-def perfume(request):
-    if request.method == "GET":
-        product = Perfume.objects.all()
-        context = {
-            'product' : product
-        }
-        return render(request, 'perfume.html', context=context)
-    return redirect(reverse('shop_page'))
-
-def home_appliances(request):
-    if request.method == "GET":
-        product = HomeAppliances.objects.all() 
-        context = {
-            'product' :  product
-        }
-        return render(request, 'home_appliances.html', context=context)
-    return redirect(reverse('shop_page'))
+def contact(request):
+    return render(request,'contact.html')
